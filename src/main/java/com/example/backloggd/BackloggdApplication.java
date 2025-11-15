9deda2f782b9c1b9a0690e75f1c991b304e3cb78
Nathaandev <nathaanprogramador@hotmail.com>
2025-11-15 19:43:14 -0300

    MySQL and SpringBoot setup

---
+++ src/main/java/com/example/backloggd/BackloggdApplication.java
@@ -0,0 +1,13 @@
+package com.example.backloggd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackloggdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackloggdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backloggd/BackloggdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backloggd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackloggdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
