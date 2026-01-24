e7f87c9ea187e9ef5378de52ed9e617ee48be34f
Nathaandev <Nathaanprogramador@hotmail.com>
2026-01-24 19:45:02 -0300

    Now when you search a game by its developers, the game is saved in the database if it isn't there already.

--- src/main/java/com/example/backloggd/Services/GameService.java
+++ src/main/java/com/example/backloggd/Services/GameService.java
@@ -83,6 +83,14 @@ public class GameService {
         RawgResponseDTO rawgResponse = rawgApiService.getGamesByDeveloper(developer, pageable);
         List<GameSummaryDTO> gamesFound = mapper.ConvertRawgResponseToGamesModel(rawgResponse);
 
+        for (GameSummaryDTO gameSummaryDTO : gamesFound){
+            Optional<GamesModel> gameOptional = gameRepository.findBygameNameIgnoreCase(gameSummaryDTO.gameName());
+            if (gameOptional.isEmpty()){
+                GamesModel game = new GamesModel();
+                BeanUtils.copyProperties(gameSummaryDTO, game);
+                gameRepository.save(game);
+            }
+        }
         return new PageImpl<>(
                 gamesFound,
                 pageable,
