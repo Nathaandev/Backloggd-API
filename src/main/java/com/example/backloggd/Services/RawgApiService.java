cd86aefaeb8fe717c6575f89211929e9eb609ad2
Nathaandev <Nathaanprogramador@hotmail.com>
2026-01-28 16:36:57 -0300

    Changed the url in GetGamesByMetacritic

--- src/main/java/com/example/backloggd/Services/RawgApiService.java
+++ src/main/java/com/example/backloggd/Services/RawgApiService.java
@@ -105,12 +105,12 @@ public class RawgApiService {
                         .block();
 
     }
-    public RawgResponseDTO getGamesByMetacritic(Integer metacritic, Pageable pageable){
+    public RawgResponseDTO getGamesByMetacritic(Pageable pageable){
         int rawgPageNumber = pageable.getPageNumber() + 1;
 
         return webClient.get()
                         .uri(uriBuilder -> uriBuilder.path("games")
-                                .queryParam("metacritic", metacritic)
+                                .queryParam("ordering", "-metacritic" )
                                 .queryParam("key", apiKey)
                                 .queryParam("page_size", "20")
                                 .queryParam("page", rawgPageNumber)
