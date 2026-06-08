48960f44664298a040d8555833cca542200f4d8d
Nathaandev <Nathaanprogramador@hotmail.com>
2026-06-08 15:31:40 -0300

    refactor user service to inject PasswordEncoder directly

--- src/main/java/com/example/backloggd/Services/UserService.java
+++ src/main/java/com/example/backloggd/Services/UserService.java
@@ -5,20 +5,21 @@ import com.example.backloggd.Models.UserModel;
 import com.example.backloggd.Repository.UserRepository;
 import com.example.backloggd.security.WebSecurityConfig;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    private final WebSecurityConfig webSecurityConfig;
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
-    public UserService(WebSecurityConfig webSecurityConfig, UserRepository userRepository) {
-        this.webSecurityConfig = webSecurityConfig;
+    public UserService(WebSecurityConfig webSecurityConfig, PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
     }
 
     public ResponseEntity<UserModel> signUp(UserRegistrationDTO userRegistrationDTO){
-        UserModel userModel = new UserModel(userRegistrationDTO.userName(), webSecurityConfig.passwordEncoder().encode(userRegistrationDTO.password()), userRegistrationDTO.userEmail());
+        UserModel userModel = new UserModel(userRegistrationDTO.userName(), passwordEncoder.encode(userRegistrationDTO.password()), userRegistrationDTO.userEmail());
         return ResponseEntity.ok(userRepository.save(userModel));
     }
 
