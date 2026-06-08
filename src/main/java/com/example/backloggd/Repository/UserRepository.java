c79196964015130761b8218dfeb9f47f615f1447
Nathaandev <Nathaanprogramador@hotmail.com>
2026-06-08 14:32:09 -0300

    UserRepository created.

---
+++ src/main/java/com/example/backloggd/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.backloggd.Repository;
+
+import com.example.backloggd.Models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+}
