2b7024879680aed63414f477fc3f68e47ad58b05
Nathaandev <Nathaanprogramador@hotmail.com>
2026-06-08 15:43:08 -0300

    implemented signup without having to be authenticated

--- src/main/java/com/example/backloggd/Services/UserService.java
+++ src/main/java/com/example/backloggd/Services/UserService.java
@@ -3,7 +3,6 @@ package com.example.backloggd.Services;
 import com.example.backloggd.DTO.UserRegistrationDTO;
 import com.example.backloggd.Models.UserModel;
 import com.example.backloggd.Repository.UserRepository;
-import com.example.backloggd.security.WebSecurityConfig;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,7 +12,7 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
-    public UserService(WebSecurityConfig webSecurityConfig, PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
     }
