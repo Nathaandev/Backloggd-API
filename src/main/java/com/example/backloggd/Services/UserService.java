e6179194f3de6e31567043142bdbb33c3302a569
Nathaandev <Nathaanprogramador@hotmail.com>
2026-06-08 15:27:57 -0300

    encoding user password

--- src/main/java/com/example/backloggd/Services/UserService.java
+++ src/main/java/com/example/backloggd/Services/UserService.java
@@ -3,19 +3,22 @@ package com.example.backloggd.Services;
 import com.example.backloggd.DTO.UserRegistrationDTO;
 import com.example.backloggd.Models.UserModel;
 import com.example.backloggd.Repository.UserRepository;
+import com.example.backloggd.security.WebSecurityConfig;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final WebSecurityConfig webSecurityConfig;
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(WebSecurityConfig webSecurityConfig, UserRepository userRepository) {
+        this.webSecurityConfig = webSecurityConfig;
         this.userRepository = userRepository;
     }
 
     public ResponseEntity<UserModel> signUp(UserRegistrationDTO userRegistrationDTO){
-        UserModel userModel = new UserModel(userRegistrationDTO.userName(), userRegistrationDTO.password(), userRegistrationDTO.userEmail());
+        UserModel userModel = new UserModel(userRegistrationDTO.userName(), webSecurityConfig.passwordEncoder().encode(userRegistrationDTO.password()), userRegistrationDTO.userEmail());
         return ResponseEntity.ok(userRepository.save(userModel));
     }
 
