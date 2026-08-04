c64610dcb2144715415ac4f2be1330169488e11a
Nathaandev <Nathaanprogramador@hotmail.com>
2026-08-04 16:37:31 -0300

    test: tidy user service test setup

--- src/test/java/com/example/backloggd/Services/UserServiceTest.java
+++ src/test/java/com/example/backloggd/Services/UserServiceTest.java
@@ -3,15 +3,12 @@ package com.example.backloggd.Services;
 import com.example.backloggd.DTO.UserRegistrationDTO;
 import com.example.backloggd.Exceptions.UsernameAlreadyInUseException;
 import com.example.backloggd.Models.UserModel;
-import com.example.backloggd.Repository.GameRepository;
-import com.example.backloggd.Repository.ReviewRepository;
 import com.example.backloggd.Repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
@@ -20,19 +17,7 @@ import static org.mockito.Mockito.when;
 class UserServiceTest {
 
     @Mock
-    private PasswordEncoder passwordEncoder;
-
-    @Mock
     private UserRepository userRepository;
-
-    @Mock
-    private GameService gameService;
-
-    @Mock
-    private GameRepository gameRepository;
-
-    @Mock
-    private ReviewRepository reviewRepository;
 
     @InjectMocks
     private UserService userService;
